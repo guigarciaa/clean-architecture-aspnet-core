0804623eded0a555c35db618d5024fe4d10ce29f
guigarciaa <guigarcia.a@outlook.com>
2023-02-07 21:54:03 -0300

    cleaned repo

--- app/CleanArch/CleanArch.Infra.Data/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CleanArch.Infra.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
